4d398408ec0a1e2655998687174a58eba7968443
PavelOlkhovoi <PavelOlkhovoi@users.noreply.github.com>
2025-03-29 16:50:42 +0000

    deploy: 16bb48933d0a8a842a2bb6d4566cd9e5c6cf51a7

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/basic-pwa/sw.js', { scope: '/basic-pwa/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
