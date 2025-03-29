4805494115530f6f71802ae8689203ff413491c4
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-03-29 18:44:34 +0100

    fix base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  base: "https://pavelolkhovoi.github.io/basic-pwa/",
+  base: "/basic-pwa/",
   plugins: [
     react(),
     VitePWA({
