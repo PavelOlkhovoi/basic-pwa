4552f4d3a4bded4ba485383c677128ebb88314ba
PavelOlkhovoi <p-olhovoi@bk.ru>
2025-03-30 12:15:06 +0200

    fetch and show users

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "6d95384581ef4199768bd424926d15b3"
   }, {
     "url": "index.html",
-    "revision": "0.b9dnefrf7v8"
+    "revision": "0.0nd2ip8ljkg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
